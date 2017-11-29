ad4f1d732dd55c06656dd24cbfd6d7e7fea0e0c5
Zeshan Munir <zmunir93@gmail.com>
2017-11-29 18:18:10 -0500

    fixed the routes page to link to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :reviews
   get '/events' => 'art_basel#index'
   get '/artwork' => 'art_basel#artwork'
-
+  get '/reviews' => 'reviews#index'
   root 'art_basel#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
